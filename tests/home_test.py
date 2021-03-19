64d27f99b46e9b305c30057401c194e371ebfe7f
YukkyHub <yukky.bryan@gmail.com>
2021-03-19 12:03:07 +0100

    up working

--- tests/home_test.py
+++ tests/home_test.py
@@ -14,9 +14,21 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
 #  Do 10 requests, waiting each time for a response
-for request in range(10):
+
+requests = [
+    b"1;74;0",
+    b"2;15;45",
+    b"2;42;15",
+    b"2;65;30",
+    b"3;8;90",
+    b"3;6;60",
+    b"1;68;0",
+    b"2;24;54",
+]
+
+for request in requests:
     print("Sending request %s …" % request)
-    socket.send(b"1;156")
+    socket.send(request)
 
     #  Get the reply.
     message = socket.recv()
